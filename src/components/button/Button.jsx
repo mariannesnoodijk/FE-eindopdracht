c212956e084f22230b71d51f686d91b848a964d6
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-10 13:46:11 +0100

    error changes to button.jsx

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './Button.css';
 import {Link} from 'react-router-dom';
 
-// const STYLES = ['btn--primary', 'btn--outline'];
-// const SIZES = ['btn--medium', 'btn--large'];
 
 export default function Button({ children, type, onClick, disabled = false, variant }) {
 
