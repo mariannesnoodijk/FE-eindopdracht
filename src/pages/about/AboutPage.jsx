7c3f961fac9283be3ec4fdda640f84a060b2d795
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-10 16:39:34 +0100

    made some changes to the about page jsx and css file

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,4 +1,6 @@
 import {useNavigate} from "react-router-dom";
+import Profilepic from '../../assets/profilepic-yellowdoor.jpg';
+
 function AboutPage() {
     // const navigate = useNavigate();
     //
@@ -7,15 +9,23 @@ function AboutPage() {
     // }
     return (
         <>
-            <div className="top-section">
+            <section className="overview-section outer-content-container">
+                <div className="inner-content-container">
+            <article className="top-section">
+               <div className="info-container">
                 <p>
-                    
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et auctor dui. Suspendisse tincidunt nisi mauris, a blandit purus accumsan eu. Phasellus aliquet congue interdum. In id nibh sit amet lectus maximus iaculis eget sit amet lacus. Sed tempus viverra dolor a malesuada. Aenean scelerisque sollicitudin vehicula. Quisque rhoncus tellus ut dictum hendrerit.
                 </p>
-                <span><img src="" alt=""/></span>
-            </div>
+               </div>
+           <span className="img-wrapper">
+               <img src={Profilepic} alt="Profilepic of yellow door" width="100%"/>
+           </span>
+            </article>
             <div className="bottom-section">
                 
             </div>
+                </div>
+            </section>
         </>
     );
 }
