2e2d6eaf81d9db19944259f2280045ff37f9ee34
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-10 16:38:52 +0100

    made some changes to propertycard jsx and css

--- src/components/propertycard/PropertyCard.jsx
+++ src/components/propertycard/PropertyCard.jsx
@@ -3,18 +3,16 @@ import {Link, useNavigate} from 'react-router-dom';
 import Button from "../button/Button.jsx";
 import './PropertyCard.css'
 
-function PropertyCard({ label, path, image, title, price, description }) {
+function PropertyCard({ label, image, title, price, description }) {
     const navigate = useNavigate();
 
-        // path, label, image, imageAlt, address, housenumber, price
-
         return(
-            <article className="propertycard">
+            <article className="property-card">
                 <span>{label}</span>
                 <img src={image} alt={title}/>
-                <h2 className="propertycard-name">{title}</h2>
+                <h2 className="property-card-name">{title}</h2>
                 <h4>€{price},-</h4>
-                <p className="propertycard-description">{description}</p>
+                <p className="property-card-description">{description}</p>
                 <Button type="button" variant="primary" onClick={() => navigate('/properties/{propertyId}')}>more info</Button>
             </article>
         );
