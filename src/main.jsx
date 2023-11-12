571fe5a0bc21ee837cb03b7e881f59f249943e17
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-12 20:33:58 +0100

    changed the AuthContext.Provider wrapper to match AuthContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom';
-import {AuthContext} from "./context/AuthContext.jsx";
+import AuthContextProvider, {AuthContext} from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Router>
-    <AuthContext.Provider>
+    <AuthContextProvider>
     <App />
-    </AuthContext.Provider>
+    </AuthContextProvider>
       </Router>
   </React.StrictMode>,
 )
