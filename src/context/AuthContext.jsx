f34af449c948223c2f5a60662625a256144522e4
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-12 19:16:35 +0100

    created setup for AuthContext.jsx

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,14 @@
+import {createContext} from "react";
+
+
+export const AuthContext = createContext({});
+
+function AuthContextProvider({ children }) {
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default AuthContextProvider;
