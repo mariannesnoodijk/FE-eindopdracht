9c41105676f5f6ffd6dc868ac2df79abdb1af182
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-14 17:58:29 +0100

    implemented authcontext, cant test yet as waiting on help

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -8,19 +8,18 @@ import {useNavigate} from 'react-router-dom'
 import InteriorImage from "../../assets/interior.jpg";
 import {AuthContext} from "../../context/AuthContext.jsx";
 
-function Login() {
+function LoginPage() {
     const [formState, setFormState] = useState({
         username: '',
         password: '',
     });
     const [submitSuccessId, setSubmitSuccessId] = useState(null);
-    const [error, toggleError] = useState(false);
-    const [loading, toggleLoading] = useState(false);
-
-    const {login} = useContext(AuthContext);
-
-    const navigate = useNavigate()
-
+//     const [loading, toggleLoading] = useState(false);
+//     const [error, toggleError] = useState(false);
+//     const {login} = useContext(AuthContext);
+//
+//     const navigate = useNavigate()
+//
     function handleChange(e) {
         setFormState({
             ...formState,
@@ -28,39 +27,65 @@ function Login() {
         })
     }
 
+//
+//     async function handleSubmit(e) {
+//         e.preventDefault();
+//         toggleError(false);
+//
+//         try {
+//             const response = await axios.post('http://localhost:8080/auth', {
+//                 ...formState
+//             });
+//             console.log(response.data.accessToken);
+//         } catch (e) {
+//             console.error(e)
+//         }
+//
+//         login();
+// toggleError(false);
+//
+// console.log({
+//     ...formState
+// });
+//
+// try {
+//     const response = await axios.post('http://localhost:8080/auth', {
+//         ...formState
+//     });
+//     console.log(response.data);
+//     // navigate('/')
+//
+//
+//     console.log('You are successfully logged in.');
+//     setSubmitSuccessId(response.data.id);
+// } catch (e) {
+//     console.error(e);
+//     toggleError(true);
+// }
+// }
+
+// function Login() {
+    const {login} = useContext(AuthContext);
+
+    const navigate = useNavigate();
     async function handleSubmit(e) {
         e.preventDefault();
 
         try {
             const response = await axios.post('http://localhost:8080/auth', {
-                ...formState
+                email: '',
+                // username : '',
+                password: '',
+
             });
             console.log(response.data.accessToken);
-        } catch (e) {
-            console.error(e)
-        }
+            login(response.data.accessToken);
 
-        login();
-        // toggleError(false);
-        //
-        // console.log({
-        //     ...formState
-        // });
-        //
-        // try {
-        //     const response = await axios.post('http://localhost:8080/auth', {
-        //         ...formState
-        //     });
-        //     console.log(response.data);
-        //     // navigate('/')
-        //
-        //
-        //     console.log('You are successfully logged in.');
-        //     setSubmitSuccessId(response.data.id);
-        // } catch (e) {
-        //     console.error(e);
-        //     toggleError(true);
-        // }
+            navigate('/profile');
+
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     return (
@@ -71,39 +96,39 @@ function Login() {
                         <h1>Login</h1>
                     </div>
 
-                    {!submitSuccessId ?
-                        <form className="general-form" onSubmit={handleSubmit}>
-                            <img src={InteriorImage} alt="Image of the interior of a home"/>
-                            <h1>welcome back</h1>
-                            <Input
-                                type="text"
-                                name="username"
-                                labelText="username"
-                                required={true}
-                                formStateValue={formState.username}
-                                handleChange={handleChange}
-                            />
-                            <Input
-                                type="password"
-                                name="password"
-                                labelText="password"
-                                required={true}
-                                formStateValue={formState.password}
-                                handleChange={handleChange}
-                            />
+                    {/*{!submitSuccessId ?*/}
+                    <form className="general-form" onSubmit={handleSubmit}>
+                        <img src={InteriorImage} alt="Image of the interior of a home"/>
+                        <h1>welcome back</h1>
+                        <Input
+                            type="text"
+                            name="username"
+                            labelText="username"
+                            required={true}
+                            formStateValue={formState.username}
+                            handleChange={handleChange}
+                        />
+                        <Input
+                            type="password"
+                            name="password"
+                            labelText="password"
+                            required={true}
+                            formStateValue={formState.password}
+                            handleChange={handleChange}
+                        />
 
-                            <Button type="submit" variant="primary">login</Button>
-                            <div className="sub-message">
-                                <p>Are you new on this page? <Link to="/accounts">Register an account first</Link></p>
-                                <p><Link to='/'>Bring me back home</Link></p>
-                            </div>
-                            {error && <p>Something went wrong with your login. Please try again.</p>}
-                        </form>
-                        : <p>Your login was successful.</p>}
+                        <Button type="submit" variant="primary">login</Button>
+                        <div className="sub-message">
+                            <p>Are you new on this page? <Link to="/accounts">Register an account first</Link></p>
+                            <p><Link to='/'>Bring me back home</Link></p>
+                        </div>
+                        {/*{error && <p>Something went wrong with your login. Please try again.</p>}*/}
+                    </form>
+                    {/*: <p>Your login was successful.</p>}*/}
                 </div>
             </section>
         </>
     );
 }
 
-export default Login;
+export default LoginPage;
