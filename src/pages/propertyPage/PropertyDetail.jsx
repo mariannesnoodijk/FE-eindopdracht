4f4a5cb61bdea83c379c36ead7995478b3c54ca6
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-16 15:00:30 +0100

    made some changes to the propertydetail pages. not fully working, need some help

--- src/pages/propertyPage/PropertyDetail.jsx
+++ src/pages/propertyPage/PropertyDetail.jsx
@@ -3,12 +3,15 @@ import axios from "axios";
 import PropertyCard from "../../components/propertycard/PropertyCard.jsx";
 import Button from "../../components/button/Button.jsx";
 import {useNavigate, useParams} from "react-router-dom";
+import ErrorMessage from "../../components/errorMessage/ErrorMessage.jsx";
 
-function PropertyInfoPage() {
+function PropertyDetail() {
     const [error, toggleError] = useState(false);
     const [loading, toggleLoading] = useState(false);
-    const [propertyInfo, setPropertyInfo] = useState([]);
+    const [property, setProperty] = useState([]);
+
     const navigate = useNavigate();
+
     const {propertyId} = useParams(); // pakt wat achter de / wordt gezet in de url
 
 
@@ -18,51 +21,42 @@ function PropertyInfoPage() {
 
     async function fetchPropertyInfo() {
         toggleError(false);
-        toggleLoading(true);
+        // toggleLoading(true);
 
         try {
             const response = await axios.get(`http://localhost:8080/properties/${propertyId}`);
             console.log(response.data);
-            setPropertyInfo(response.data);
+            setProperty(response.data);
         } catch (e) {
             console.error(e);
-            console.error("Error status:", e.response.status);
-            console.error("Error data:", e.response.data);
-
             toggleError(true);
         }
-        toggleLoading(false);
+        // toggleLoading(false);
     }
 
     return (
         <>
-            <section className="overview-section outer-content-container">
+            <section className="property-detail-section outer-content-container">
                 <div className="inner-content-container">
                     <div className="general-form-top">
                         <h1>property detail</h1>
                     </div>
                     <div className="general-form">
-                    {Object.keys(propertyInfo).length > 0 && (
-                        // <div className="container-propertyinfo">
-                    // {propertyPage.map((property) => {
-                            <PropertyCard
-                                key={propertyInfo.propertyInfoId}
-                                // image={cardImage1}
-                                label="New"
-                                title={`${propertyInfo.address}`}
-                                price={propertyInfo.price}
-                                description={propertyInfo.description}
-                            />
-                        // </div>
-                    )}
-                    <Button type="submit" variant="primary" onClick={() => navigate('/viewingsPage')} >Afspraak maken</Button>
-                    <Button type="submit" variant="primary" onClick={() => navigate('/propertyPage')}>Terug naar overzicht</Button>
-                    {error && <p>Er is iets misgegaan bij het ophalen van de data. Probeer het opnieuw.</p>}
-                </div>
+                        {Object.keys(property).length > 0 && (
+                            <>
+                                <h1>{property.address}</h1>
+                                <h2>{property.price}</h2>
+                                <p>{property.description}</p>
+                            </>
+                        )}
+                        <Button type="button" variant="primary" onClick={() => navigate('/viewings')}>Plan your viewing</Button>
+                        <Button type="button" variant="primary" onClick={() => navigate('/properties')}>Back to properties</Button>
+                        {error && <ErrorMessage message="Something went wrong fetching your data. Please try again."/>}
+                    </div>
                 </div>
             </section>
         </>
     );
 }
 
-export default PropertyInfoPage;
+export default PropertyDetail;
