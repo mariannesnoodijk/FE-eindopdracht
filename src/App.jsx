e80723bfea94e18d2c7a037f82c3cd0219c0f9ea
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-11-16 14:59:36 +0100

    changed some routes to new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import "./App.css";
 import {Routes, Route, Navigate} from "react-router-dom";
 
-import Homepage from "./pages/home/Homepage.jsx";
+import Home from "./pages/homePage/Home.jsx";
 import PageNotFound from "./pages/pageNotFound/PageNotFound.jsx";
-import Login from "./pages/login/LoginPage.jsx";
-import Register from "./pages/register/RegisterPage.jsx";
-import Favorites from "./pages/favorites/Favorites.jsx";
+import Login from "./pages/loginPage/Login.jsx";
+import Register from "./pages/registerPage/Register.jsx";
+import Favorites from "./pages/favoritePage/Favorites.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Navbar from "./components/navigation/Navbar.jsx";
-import Viewings from "./pages/viewings/Viewings.jsx";
+import ScheduleViewing from "./pages/scheduleViewingPage/ScheduleViewing.jsx";
 import PostProperty from "./components/postProperty/PostProperty.jsx";
-import Properties from "./pages/properties/Properties.jsx";
-import ProfilePage from "./pages/profile/ProfilePage.jsx";
-import PropertyMatching from "./pages/propertyMatching/PropertyMatching.jsx";
-import PropertyInfoPage from "./pages/properties/PropertyInfoPage.jsx";
+import Properties from "./pages/propertyPage/Properties.jsx";
+import Profile from "./pages/profilePage/Profile.jsx";
+import PropertyMatching from "./pages/propertyMatchingPage/PropertyMatching.jsx";
+import PropertyInfoPage from "./pages/propertyPage/PropertyDetail.jsx";
 import {useContext} from "react";
 import {AuthContext} from "./context/AuthContext.jsx";
+import ViewingOverview from "./pages/viewingOverviewPage/ViewingOverview.jsx";
 
 
 
@@ -26,16 +27,17 @@ const {isAuth} = useContext(AuthContext);
       <>
               <Navbar/>
               <Routes>
-                  <Route path="/" element={<Homepage/>}/>
+                  <Route path="/" element={<Home/>}/>
                   <Route path="/properties" element={<Properties/>}/>
-                  <Route path="/addingOfProperty" element={<PostProperty/>}/>
+                  <Route path="/addingAProperty" element={<PostProperty/>}/>
                   <Route path="/properties/:propertyId" element={<PropertyInfoPage/>}/>
                   <Route path="/matching" element={<PropertyMatching/>}/>
                   <Route path="/login" element={<Login/>}/>
                   <Route path="/register" element={<Register/>}/>
-                  {/*<Route path="/profile" element={<ProfilePage/>}/>*/}
-                  <Route path="/profile" element={isAuth ? <ProfilePage/> : <Navigate to="/"/>}/>
-                  <Route path="/viewings" element={<Viewings/>}/>
+                  <Route path="/profile" element={<Profile/>}/>
+                  {/*<Route path="/profile" element={isAuth ? <Profile/> : <Navigate to="/"/>}/>*/}
+                  <Route path="/viewings" element={<ScheduleViewing/>}/>
+                  <Route path="/viewingsoverview" element={<ViewingOverview/>}/>
                   <Route path="/favorites" element={<Favorites/>}/>
                   <Route path="*" element={<PageNotFound/>}/>
               </Routes>
