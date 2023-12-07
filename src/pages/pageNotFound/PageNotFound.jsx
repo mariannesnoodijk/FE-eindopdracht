bae54c5facf23b9ca36511b9ccd3b3038f77013b
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-12-07 10:12:00 +0100

    changed the text to English as was in Dutch

--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -9,7 +9,7 @@ function PageNotFound() {
         <section className="not-found__section outer-content__container">
             <div className="inner-content-container">
                 <h1>404</h1>
-                <h2>De pagina waar je naar zoekt bestaat niet</h2>
+                <h2>The page you are looking for, does not exist</h2>
                 <span>
                    <Button type="button" variant="primary" onClick={() => navigate('/')}>bring me back home</Button>
                 </span>
