b2e9a23629adfeff7f6c605b1142cfdab692fbaf
mariannesnoodijk <ams.snoodijk@gmail.com>
2023-12-07 10:27:11 +0100

    deleted some unused code. might still need some work.

--- src/pages/viewingOverviewPage/ViewingOverview.jsx
+++ src/pages/viewingOverviewPage/ViewingOverview.jsx
@@ -1,5 +1,4 @@
 import Button from "../../components/button/Button.jsx";
-import ErrorMessage from "../../components/errorMessage/ErrorMessage.jsx";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {Link, useParams} from "react-router-dom";
@@ -14,7 +13,7 @@ function ViewingOverview() {
         fetchViewings()
     }, []);
 
-    const {viewingId} = useParams(); // pakt wat achter de / wordt gezet in de url
+    const {viewingId} = useParams();
 
     async function fetchViewings() {
         toggleError(false);
@@ -22,7 +21,6 @@ function ViewingOverview() {
 
         try {
             const response = await axios.get('http://localhost:8080/viewings');
-            console.log(response.data);
             setViewing(response.data);
         } catch (e) {
             console.error(e);
@@ -37,8 +35,8 @@ function ViewingOverview() {
             toggleLoading(true);
             const response = await axios.delete(`http://localhost:8080/viewings/${viewingId}`);
             setViewing(response.data);
-        } catch (error) {
-            console.log(error);
+        } catch (e) {
+            console.error(e);
         }
     }
 
@@ -50,17 +48,6 @@ function ViewingOverview() {
                       <h1>my scheduled viewings</h1>
                   </div>
                   <div className="general-form">
-                      {/*{Object.keys(property).length > 0 && (*/}
-                      {/*    <>*/}
-                      {/*        <h1>{property.address}</h1>*/}
-                      {/*        <h2>{property.price}</h2>*/}
-                      {/*        <p>{property.description}</p>*/}
-                      {/*    </>*/}
-                      {/*)}*/}
-                      {/*<Button type="submit" variant="primary" onClick={() => navigate('/viewings')}>Plan your viewing</Button>*/}
-                      {/*<Button type="submit" variant="primary" onClick={() => navigate('/properties')}>Back to properties</Button>*/}
-                      {/*{error && <ErrorMessage message="Something went wrong fetching your data. Please try again."/>}*/}
-
                       <Button type="button" onClick={deleteViewing}>Delete viewing</Button>
                       <div className="sub-message">
                           <p><Link className="sub-message__links" to='/profile'>Take me to the previous page</Link></p>
